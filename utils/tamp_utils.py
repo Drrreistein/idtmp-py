abfd144e7ab6ff6f9436f44170464346f20f87e6
Lei Xu <lei.xu@rvbust.com>
2021-12-04 09:19:40 +0800

    update

---
+++ utils/tamp_utils.py
@@ -0,0 +1,20 @@
+import pybullet as p
+
+def display_text(loc, text):
+
+    pass
+
+class TextDisplayer(object):
+    def __init__(self, textPosition):
+        self.textPosition = textPosition
+        self.max_lines = 10
+
+    def display_text(text):
+        p.addUserDebugText(text)
+        pass
+
+    def roll_lines():
+        pass
+
+    def remove_all_text():
+        pass
